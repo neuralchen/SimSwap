d301f43537b0966c1df63010a3583065598381b7
NNNNAI <844294823@qq.com>
2021-07-02 21:34:11 +0800

    Script for selecting the specific person to be swapped in image swapping
    Script for selecting the specific person to be swapped in image swapping

---
+++ test_wholeimage_swapspecific.py
@@ -0,0 +1,124 @@
+
+import cv2
+import torch
+import fractions
+import numpy as np
+from PIL import Image
+import torch.nn.functional as F
+from torchvision import transforms
+from models.models import create_model
+from options.test_options import TestOptions
+from insightface_func.face_detect_crop_mutil import Face_detect_crop
+from util.reverse2original import reverse2wholeimage
+import os
+from util.add_watermark import watermark_image
+import torch.nn as nn
+from util.norm import SpecificNorm
+
+def lcm(a, b): return abs(a * b) / fractions.gcd(a, b) if a and b else 0
+
+transformer_Arcface = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+def _totensor(array):
+    tensor = torch.from_numpy(array)
+    img = tensor.transpose(0, 1).transpose(0, 2).contiguous()
+    return img.float().div(255)
+
+def _toarctensor(array):
+    tensor = torch.from_numpy(array)
+    img = tensor.transpose(0, 1).transpose(0, 2).contiguous()
+    return img.float().div(255)
+
+if __name__ == '__main__':
+    opt = TestOptions().parse()
+
+    start_epoch, epoch_iter = 1, 0
+    crop_size = 224
+
+    torch.nn.Module.dump_patches = True
+    logoclass = watermark_image('./simswaplogo/simswaplogo.png')
+    model = create_model(opt)
+    model.eval()
+    mse = torch.nn.MSELoss().cuda()
+
+    spNorm =SpecificNorm()
+
+
+    app = Face_detect_crop(name='antelope', root='./insightface_func/models')
+    app.prepare(ctx_id= 0, det_thresh=0.8, det_size=(640,640))
+
+    pic_a = opt.pic_a_path
+    pic_specific = opt.pic_specific_path
+
+    # The person who provides id information 
+    img_a_whole = cv2.imread(pic_a)
+    img_a_align_crop, _ = app.get(img_a_whole,crop_size)
+    img_a_align_crop_pil = Image.fromarray(cv2.cvtColor(img_a_align_crop[0],cv2.COLOR_BGR2RGB)) 
+    img_a = transformer_Arcface(img_a_align_crop_pil)
+    img_id = img_a.view(-1, img_a.shape[0], img_a.shape[1], img_a.shape[2])
+
+    # convert numpy to tensor
+    img_id = img_id.cuda()
+
+    #create latent id
+    img_id_downsample = F.interpolate(img_id, scale_factor=0.5)
+    latend_id = model.netArc(img_id_downsample)
+    latend_id = F.normalize(latend_id, p=2, dim=1)
+
+
+    # The specific person to be swapped
+    specific_person_whole = cv2.imread(pic_specific)
+    specific_person_align_crop, _ = app.get(specific_person_whole,crop_size)
+    specific_person_align_crop_pil = Image.fromarray(cv2.cvtColor(specific_person_align_crop[0],cv2.COLOR_BGR2RGB)) 
+    specific_person = transformer_Arcface(specific_person_align_crop_pil)
+    specific_person = specific_person.view(-1, specific_person.shape[0], specific_person.shape[1], specific_person.shape[2])
+
+    # convert numpy to tensor
+    specific_person = specific_person.cuda()
+
+    #create latent id
+    specific_person_downsample = F.interpolate(specific_person, scale_factor=0.5)
+    specific_person_id_nonorm = model.netArc(specific_person_downsample)
+    # specific_person_id_norm = F.normalize(specific_person_id_nonorm, p=2, dim=1)
+
+    ############## Forward Pass ######################
+
+    pic_b = opt.pic_b_path
+    img_b_whole = cv2.imread(pic_b)
+
+    img_b_align_crop_list, b_mat_list = app.get(img_b_whole,crop_size)
+    # detect_results = None
+    swap_result_list = []
+
+    id_compare_values = [] 
+    b_align_crop_tenor_list = []
+    for b_align_crop in img_b_align_crop_list:
+
+        b_align_crop_tenor = _totensor(cv2.cvtColor(b_align_crop,cv2.COLOR_BGR2RGB))[None,...].cuda()
+
+        b_align_crop_tenor_arcnorm = spNorm(b_align_crop_tenor)
+        b_align_crop_tenor_arcnorm_downsample = F.interpolate(b_align_crop_tenor_arcnorm, scale_factor=0.5)
+        b_align_crop_id_nonorm = model.netArc(b_align_crop_tenor_arcnorm_downsample)
+
+        id_compare_values.append(mse(b_align_crop_id_nonorm,specific_person_id_nonorm).detach().cpu().numpy())
+        b_align_crop_tenor_list.append(b_align_crop_tenor)
+
+    id_compare_values_array = np.array(id_compare_values)
+    min_index = np.argmin(id_compare_values_array)
+    min_value = id_compare_values_array[min_index]
+
+    if min_value < opt.id_thres:
+
+        swap_result = model(None, b_align_crop_tenor_list[min_index], latend_id, None, True)[0]
+
+        reverse2wholeimage([swap_result], [b_mat_list[min_index]], crop_size, img_b_whole, logoclass, os.path.join(opt.output_path, 'result_whole_swapspecific.jpg'), opt.no_simswaplogo)
+
+        print(' ')
+
+        print('************ Done ! ************')
+
+    else:
+        print('The person you specified is not found on the picture: {}'.format(pic_b))
