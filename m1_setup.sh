7ea8d1e0013e0a1c639d3e52ae0be8d8e54b70e8
Smiril <Smiril@users.noreply.github.com>
2022-07-09 22:38:55 +0200

    Update m1_setup.sh

--- m1_setup.sh
+++ m1_setup.sh
@@ -31,24 +31,7 @@ python3 -m pip install torch
 python3 -m pip install image
 python3 -m pip install timm
 python3 -m pip install PlL
-python3 -m pip install wrapt==1.12.1
-python3 -m pip install opt_einsum
-python3 -m pip install flatbuffers==1.12.0
-python3 -m pip install google-pasta==0.2
-python3 -m pip install h5py==3.1.0
-python3 -m pip install keras-nightly==2.5.0.dev2021032900
-python3 -m pip install keras-preprocessing==1.1.2
-python3 -m pip install tensorflow-estimator<2.6.0,>=2.5.0rc0
-python3 -m pip install termcolor==1.1.0
-python3 -m pip install absl-py==0.10
-python3 -m pip install gast==0.4.0
-python3 -m pip install grpcio==1.34.0
-python3 -m pip install typing-extensions==3.7.4
-python3 -m pip install astunparse==1.6.3
-conda install -c apple tensorflow-deps==2.5.0
-python3 -m pip install tensorflow-macos==2.5.0
-python3 -m pip install tensorflow-macos==2.5.0 --no-dependencies
-python3 -m pip install tensorflow-metal==0.1.2
+python3 -m pip install h5py==3.7.0
 
 python3 m1_test.py
 for i in `seq 1 6`; do
