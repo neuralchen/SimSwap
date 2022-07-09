ed033c24f64c3699b7cad5d6244821b3edd41b9c
Smiril <Smiril@users.noreply.github.com>
2022-07-09 16:43:42 +0200

    Create m1_setup.sh
    simple setup steps for M1 Apple GPU

---
+++ m1_setup.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+conda create -n torch-gpu python=3.9
+conda activate torch-gpu
+conda install pytorch torchvision torchaudio -c pytorch-nightly
+conda install -c conda-forge jupyter jupyterlab
+python3 -m pip install numpy
+python3 -m pip install torch
+python3 -m pip install image
+python3 -m pip install timm
+python3 -m pip install PlL
+python3 m1_test.py
+
