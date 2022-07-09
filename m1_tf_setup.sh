6c25f608b1e5a7ee0c57de43de52a2eee9d64425
Smiril <Smiril@users.noreply.github.com>
2022-07-09 22:37:25 +0200

    Update m1_tf_setup.sh

--- m1_tf_setup.sh
+++ m1_tf_setup.sh
@@ -4,12 +4,11 @@ conda init --all
 conda create --name tensorflow_m1 python==3.9
 conda activate tensorflow_m1
 conda install pytorch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0 -c pytorch
-
 python3 -m pip install onnxruntime-gpu
 python3 -m pip install insightface==0.2.1 onnxruntime moviepy
 python3 -m pip install googledrivedownloader
 python3 -m pip install imageio==2.4.1
-python3 -m pip install numpy==1.19.2
+python3 -m pip install numpy==1.23.1
 python3 -m pip install torch
 python3 -m pip install image
 python3 -m pip install timm
@@ -18,7 +17,7 @@ python3 -m pip install wrapt==1.12.1
 python3 -m pip install opt_einsum
 python3 -m pip install flatbuffers==1.12.0
 python3 -m pip install google-pasta==0.2
-python3 -m pip install h5py==3.1.0
+python3 -m pip install h5py==3.7.0
 python3 -m pip install keras-nightly==2.5.0.dev2021032900
 python3 -m pip install keras-preprocessing==1.1.2
 python3 -m pip install tensorflow-estimator<2.6.0,>=2.5.0rc0
@@ -28,9 +27,9 @@ python3 -m pip install gast==0.4.0
 python3 -m pip install grpcio==1.34.0
 python3 -m pip install typing-extensions==3.7.4
 python3 -m pip install astunparse==1.6.3
-conda install -c apple tensorflow-deps==2.5.0
-python3 -m pip install tensorflow-macos==2.5.0
-python3 -m pip install tensorflow-macos==2.5.0 --no-dependencies
+conda install -c apple tensorflow-deps==2.6.0
+python3 -m pip install tensorflow-macos==2.6.0
+python3 -m pip install tensorflow-macos==2.6.0 --no-dependencies
 python3 -m pip install tensorflow-metal==0.1.2
 
 python3 m1_tf_test.py
