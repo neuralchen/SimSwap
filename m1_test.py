2043ee6e0b3a83ce974f8bdfe56ac3b3cd3f3fc9
Smiril <Smiril@users.noreply.github.com>
2022-07-09 16:29:33 +0200

    Create m1_test.py
    some apple GPU Tests with math and PyTorch

---
+++ m1_test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+#############################################################
+# File: m1_test.py
+# Created Date: Saturday July 9th 2022
+# Author: Smiril
+# Email: sonar@gmx.com
+#############################################################
+
+import torch
+import math
+# this ensures that the current MacOS version is at least 12.3+
+print(torch.backends.mps.is_available())
+# this ensures that the current current PyTorch installation was built with MPS activated.
+print(torch.backends.mps.is_built())
+dtype = torch.float
+device = torch.device("mps")
+
+# Create random input and output data
+x = torch.linspace(-math.pi, math.pi, 2000, device=device, dtype=dtype)
+y = torch.sin(x)
+
+# Randomly initialize weights
+a = torch.randn((), device=device, dtype=dtype)
+b = torch.randn((), device=device, dtype=dtype)
+c = torch.randn((), device=device, dtype=dtype)
+d = torch.randn((), device=device, dtype=dtype)
+
+learning_rate = 1e-6
+for t in range(2000):
+    # Forward pass: compute predicted y
+    y_pred = a + b * x + c * x ** 2 + d * x ** 3
+
+    # Compute and print loss
+    loss = (y_pred - y).pow(2).sum().item()
+    if t % 100 == 99:
+        print(t, loss)
+
+# Backprop to compute gradients of a, b, c, d with respect to loss
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_a = grad_y_pred.sum()
+    grad_b = (grad_y_pred * x).sum()
+    grad_c = (grad_y_pred * x ** 2).sum()
+    grad_d = (grad_y_pred * x ** 3).sum()
+
+    # Update weights using gradient descent
+    a -= learning_rate * grad_a
+    b -= learning_rate * grad_b
+    c -= learning_rate * grad_c
+    d -= learning_rate * grad_d
+
+
+print(f'Result: y = {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3')
+
